79e9aca0ad552284131f13242266f8f7a5114acf
zuzanna.bogus zuzanna.bogus@vaimo.com <zuzanna.bogus@vaimo.com>
2024-07-08 09:31:34 +0200

    html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -14,5 +15,12 @@ module.exports = {
               use: ['style-loader', 'css-loader']  
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 }
