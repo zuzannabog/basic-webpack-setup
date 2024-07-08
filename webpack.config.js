60611afc9406904f93fd77a3e2093221b01c7c91
zuzanna.bogus zuzanna.bogus@vaimo.com <zuzanna.bogus@vaimo.com>
2024-07-08 11:36:16 +0200

    CSS Minify Extract Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
@@ -22,7 +23,7 @@ module.exports = {
         rules: [
             {
               test: /\.css$/,
-              use: ['style-loader', 'css-loader']  
+              use: [MiniCssExtractPlugin.loader, "css-loader"]
             },
             {
                 test: /\.js$/,
@@ -41,6 +42,7 @@ module.exports = {
             title: 'Webpack App',
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin(),
     ]
 }
